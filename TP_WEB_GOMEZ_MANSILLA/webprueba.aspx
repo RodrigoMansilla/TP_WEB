f76a935890bafcf91f2c57f10f552b05d2bc92d0
carlos gabriel gomez <gomezkarlos1988@hotmail.com>
2019-05-19 19:14:42 -0300

    agregue una clase

---
+++ TP_WEB_GOMEZ_MANSILLA/webprueba.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="webprueba.aspx.cs" Inherits="TP_WEB_GOMEZ_MANSILLA.webprueba" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <asp:Button ID="bttn_prueba" runat="server" Text="Button" />
+
+
+
+</asp:Content>
---
+++ TP_WEB_GOMEZ_MANSILLA/webprueba.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TP_WEB_GOMEZ_MANSILLA
+{
+    public partial class webprueba : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
